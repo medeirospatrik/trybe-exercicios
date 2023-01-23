3f7b072315e5034eacb66ae7e9a676b80e3e2333
medeirospatrik <medeirospatrik2@gmail.com>
2023-01-23 16:15:29 -0300

    Exercicio 3 do conteudo assicrono, if e else

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javaScript-primeiros-passos/scripts.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javaScript-primeiros-passos/scripts.js
@@ -1,4 +1,4 @@
-const myName = "Patrik";
+/*const myName = "Patrik";
 const birthCity = "Paracambi";
 let birthyear = 2030;
 
@@ -6,13 +6,25 @@ console.log(myName, birthCity, birthyear);
 
 birthCity = "São paulo"
 
-console.log(birthCity)
+console.log(birthCity)*/
 
 // Exercicios 2
 
-const base = 5;
+/*const base = 5;
 const height = 8;
 const area = base * height;
 const perimeter = (base * 2) + (height * 2); 
 
-console.log(area)
+console.log(area)*/
+
+// Exercicios 3 if e else
+
+const notaMaria = 50;
+
+if (notaMaria >= 80){
+    console.log("Parabens voce foi aprovada!!")
+}else if (notaMaria < 80 && notaMaria >= 60) {
+    console.log("Você esta na nossa lista de espera.")
+}else if ( notaMaria < 60){
+    console.log("Infelizmente você reprovou.")
+}
