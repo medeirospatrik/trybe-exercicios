3122e4a72761fe99ef728d3ac2a6cf14949cde4f
medeirospatrik <medeirospatrik2@gmail.com>
2023-01-23 18:44:10 -0300

    EXERCICIO 4 SWITCH CASE

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javaScript-primeiros-passos/scripts.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javaScript-primeiros-passos/scripts.js
@@ -19,12 +19,35 @@ console.log(area)*/
 
 // Exercicios 3 if e else
 
-const notaMaria = 50;
+// const notaMaria = 50;
 
-if (notaMaria >= 80){
+/* if (notaMaria >= 80){
     console.log("Parabens voce foi aprovada!!")
-}else if (notaMaria < 80 && notaMaria >= 60) {
+ }else if (notaMaria < 80 && notaMaria >= 60) {
     console.log("Você esta na nossa lista de espera.")
 }else if ( notaMaria < 60){
     console.log("Infelizmente você reprovou.")
+}*/
+
+// Exercicios 4 switch case
+
+let statusNoProcesso = 'reprovada'
+
+switch (statusNoProcesso){
+    case 'aprovada':
+       console.log(" Parabéns voce foi aprovada(o)")
+    break;
+
+    case 'lista' :
+        console.log("Você esta na nossa lista de espera")
+        break;
+
+    case 'reprovada' :
+        console.log('Você foi reprovado')
+        break;
+
+    default :
+    console.log("Informação incorreta")
+
+
 }
