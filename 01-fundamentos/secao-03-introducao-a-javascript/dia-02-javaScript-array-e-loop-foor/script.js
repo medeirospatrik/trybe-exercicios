881277442551a11ca34d097a7375c33579d30fba
medeirospatrik <medeirospatrik2@gmail.com>
2023-01-25 00:50:26 -0300

    evoluindo nos exercicios do dia dois

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javaScript-array-e-loop-foor/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javaScript-array-e-loop-foor/script.js
@@ -22,7 +22,7 @@ for(let index = 0; index < numbers.length; index += 1){
  
 }
 let media = resultado / numbers.length
-console.log(`A media dos numeros do array numbers é ${media}.`)
+console.log(`A media dos numeros do array numbers é  ${media}.`)
 
 if (media > 20 ){
     console.log("Valor maior que 20.")
@@ -33,9 +33,14 @@ if (media > 20 ){
 //Descobrindo o maior valor do array
 
 //Descobrindo o maior numero
+let maiorNumero = 0
+
 for(let index = 0; index < numbers.length; index += 1){
-    
+    if (maiorNumero < numbers[index]){
+      maiorNumero = numbers[index]
+    }
   }
+  console.log(`o maior numero do array number é ${maiorNumero}`)
 
   //descobrindo numero impares
   let numbersImpar = 0
