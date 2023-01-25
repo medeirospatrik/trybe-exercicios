4969057c683a5abd8214904607ceae01a529ff8e
medeirospatrik <medeirospatrik2@gmail.com>
2023-01-25 09:46:03 -0300

    primeira parte do exercicio do dia 01 de introdução ao java script

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-introducao-a-javaScript/script.js
@@ -0,0 +1,14 @@
+let a = 40;
+let b = 10;
+
+let sum = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+let rest = a % b;
+
+console.log(sum)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(rest)
