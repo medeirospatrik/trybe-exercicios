fbe6123a4cb0ffa932f05bfdf485637cb9bbdad0
medeirospatrik <medeirospatrik2@gmail.com>
2023-01-26 09:22:56 -0300

    Finalizando o exercicio normal do dia 03 de java script logica de programação

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javaScript-logica-de-programação/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javaScript-logica-de-programação/script.js
@@ -14,3 +14,49 @@ for(let index = word.length - 1; index >= 0; index -=1){
 inverseWord += word[index]
 }
 console.log(inverseWord)
+
+//Imprimindo a maior e a menor palavra do array!
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let bigWord = array[0]
+let smallWord = array[0]
+
+
+for(let index = 0; index < array.length; index += 1){
+        if(bigWord.length < array[index].length){
+            bigWord = array[index]
+    }
+}
+console.log(bigWord)
+
+
+for(let index = 0; index < array.length; index += 1){
+    if(smallWord.length > array[index].length){
+        smallWord = array[index]
+    }
+}
+console.log(smallWord)
+
+// O maior numero primo entre 2 e 50 
+
+let numerosPrimos = [0]
+let maiorNumero = [0]
+
+
+for(let index =2; index <= 50; index += 1){
+    if(index % index === 0 && index % 2 !== 0){
+            numerosPrimos.push(index)
+ }
+ for(let index2 = 0; index2 < numerosPrimos.length; index2 +=1){
+    if(maiorNumero < numerosPrimos[index2]){
+        maiorNumero[0] = numerosPrimos[index2]
+    }
+
+ }
+ 
+ } 
+
+ console.log(`os numeros primos entre 2 e 50 são ${numerosPrimos}`)
+ 
+console.log(`O maior numero primo entre 2 e 50 é ${maiorNumero}`);
+  
