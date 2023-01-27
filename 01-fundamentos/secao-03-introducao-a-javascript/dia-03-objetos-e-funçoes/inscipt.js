7a77da931e51b35da6ecc508e470d49b62a78c87
medeirospatrik <medeirospatrik2@gmail.com>
2023-01-27 00:18:49 -0300

    finalizando os exercicios sobre for in e objects

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-03-objetos-e-funçoes/inscipt.js
@@ -0,0 +1,57 @@
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// console.log(`Bem-vinda, ${info.personagem}`)
+
+// info.recorrente = 'sim';
+
+// console.log(info)
+
+// for(let chaves in info){
+//     console.log(chaves)
+// }
+
+// for(let chaves in info){
+//     console.log(info[chaves])
+// }
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'sim',
+
+//   };
+
+//   console.log(info.personagem + ' e ' + info2.personagem)
+//   console.log(info.origem + ' e ' + info2.origem)
+//   console.log(info.nota + ' ' + info2.nota)
+//   if(info.recorrente === info2.recorrente){
+//     console.log('Ambos recorrentes')
+//   }
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
+
+//   console.log(`o livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo} `)
+  
+//   leitor.livrosFavoritos.push({
+//     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//     autor: 'JK Rowling',
+//     editora: 'Rocco',
+//   })
+
+//   console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos.`)
