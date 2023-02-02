dd23be34244ee10e8f5270a9bc031d5f6a7f6faa
medeirospatrik <medeirospatrik2@gmail.com>
2023-02-01 23:38:12 -0300

    exercicios do conteudo assincrono

---
+++ 01-fundamentos/secao-04-javaScript-dom-eventos-e-web-store/exercicios-dom-javascript-dom-e-seletores-4.2/script.js
@@ -0,0 +1,10 @@
+console.log(document.querySelector('#primeiroFilho').nextElementSibling)
+const pai =document.querySelector('#pai')
+document.querySelector('#elementoOndeVoceEsta').parentElement.style.backgroundColor = 'red';
+'red'
+document.querySelector("#pai").firstElementChild.innerHTML = 'Patrik Medeiros'
+console.log(document.querySelector("#pai").firstElementChild)
+console.log(document.querySelector("#elementoOndeVoceEsta").previousElementSibling)
+console.log(document.querySelector("#elementoOndeVoceEsta").nextSibling)
+console.log(document.querySelector("#elementoOndeVoceEsta").nextElementSibling)
+console.log(document.querySelector('#pai').lastElementChild.previousElementSibling)
