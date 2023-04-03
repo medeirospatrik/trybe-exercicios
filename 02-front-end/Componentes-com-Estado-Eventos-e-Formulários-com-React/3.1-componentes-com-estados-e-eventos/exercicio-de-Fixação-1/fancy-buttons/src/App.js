af3d90c31880919c72378379fc18757420985409
Patrik Medeiros <medeirospatrik2@gmail.com>
2023-04-03 15:35:28 -0300

    refatorando, colocando a função dentro da classe usando o this.

--- 02-front-end/Componentes-com-Estado-Eventos-e-Formulários-com-React/3.1-componentes-com-estados-e-eventos/exercicio-de-Fixação-1/fancy-buttons/src/App.js
+++ 02-front-end/Componentes-com-Estado-Eventos-e-Formulários-com-React/3.1-componentes-com-estados-e-eventos/exercicio-de-Fixação-1/fancy-buttons/src/App.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
 
-const handleClick = () => console.log('Eu sou o botão 1');
-const handleClick2 = () => console.log('Eu sou o botão 2');
-const handleClick3 = () => console.log('Eu sou o botão 3');
+
 class App extends Component {
+ 
+   handleClick = () => {
+    console.log(this)
+    console.log('Eu sou o botão 1');
+  }
+   handleClick2 = () => console.log('Eu sou o botão 2');
+  handleClick3 = () => console.log('Eu sou o botão 3');
   render() {
     return(
       <div>
-        <button onClick={handleClick}>Meu botão1</button>
-        <button onClick={handleClick2}>Meu botão1</button>
-        <button onClick={handleClick3}>Meu botão1</button>
+        <button onClick={this.handleClick}>Meu botão1</button>
+        <button onClick={this.handleClick2}>Meu botão1</button>
+        <button onClick={this.handleClick3}>Meu botão1</button>
       </div>
     )
   }
