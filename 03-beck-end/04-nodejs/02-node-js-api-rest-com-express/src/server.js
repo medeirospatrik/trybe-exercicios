545575809e736ffdefed75b32b70b99c0dcc1c85
Patrik Medeiros <medeirospatri2@gmail.com>
2023-07-26 23:59:12 -0300

    criando metodo put e delete

--- 03-beck-end/04-nodejs/02-node-js-api-rest-com-express/src/server.js
+++ 03-beck-end/04-nodejs/02-node-js-api-rest-com-express/src/server.js
@@ -10,6 +10,10 @@ const read = async () => {
   }
 }
 
+const werite = (movies,newFilm) => {
+  fs.writeFile('src/data/movies.json', JSON.stringify([...movies, {id: movies.length + 1, ...newFilm}]))
+}
+
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
 
@@ -23,6 +27,48 @@ app.get('/movies/:id', async (req, res) => {
 app.get('/movies', async (req, res) => {
   const movies = await read()
   res.status(200).json(movies)
+})
+
+app.post('/movies', async (req, res) => {
+  const newFIlm = req.body;
+  const movies = await read();
+  werite(movies, newFIlm)
+  res.status(201).json(newFIlm)
+})
+
+app.put('/movies/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+  const update = req.body;
+  const movies = await read();
+
+  const updateMovies = movies.map((movi) => {
+    if (movi.id === Number(id)) {
+      {
+        movi = {id: Number(id),...update}
+      }
+
+
+    }
+    return movi
+  })
+  fs.writeFile('src/data/movies.json', JSON.stringify(updateMovies))
+  res.status(220).send(updateMovies)
+  } catch(err) {console.log(err.message)}
+  
+})
+
+app.delete('/movies/:id', async (req, res) => {
+  try{
+    const movies = await read()
+    const { id } = req.params;
+    const newMovies = movies.filter((movie) => movie.id !== Number(id))
+    fs.writeFile('src/data/movies.json', JSON.stringify(newMovies))
+    res.status(404).end()
+  } catch (err) {
+    console.log(err.message);
+  }
+  
 })
 
 app.listen('3000', () => console.log('estou rodando'))
