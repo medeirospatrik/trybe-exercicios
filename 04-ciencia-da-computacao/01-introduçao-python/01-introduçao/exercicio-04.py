145bfb24e0914121effc3c0082bed16d7d7618df
Patrik Medeiros <medeirospatri2@gmail.com>
2024-02-22 04:46:45 -0300

    requisito 4 ok

---
+++ 04-ciencia-da-computacao/01-introduçao-python/01-introduçao/exercicio-04.py
@@ -0,0 +1,14 @@
+# 🚀 Exercício 4: Crie uma função que receba uma lista de nomes e retorne o nome com a maior quantidade de caracteres. Por exemplo, para ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"], o retorno deve ser "Fernanda".
+
+nome = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]
+
+def maior_nome(names: list):
+    big_name = ''
+    for name in names:
+        if len(name) > len(big_name):
+            big_name = name
+    
+    print(big_name)
+
+maior_nome(nome)
+
