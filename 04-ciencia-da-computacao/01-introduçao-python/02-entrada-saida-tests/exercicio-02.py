9aa5c18bcfc63634711af7e439655b897c342fea
Patrik Medeiros <medeirospatri2@gmail.com>
2024-02-24 04:49:58 -0300

    exercicio 2 ok

---
+++ 04-ciencia-da-computacao/01-introduçao-python/02-entrada-saida-tests/exercicio-02.py
@@ -0,0 +1,19 @@
+import random
+
+lista_de_palavras = ['carro', 'casa', 'igreja']
+palavra_sorteada = random.choice(lista_de_palavras)
+embaralhada = ''.join(random.sample(palavra_sorteada, len(palavra_sorteada)))
+
+counter = 1
+
+while palavra_sorteada:
+    tentativa = input(f'Qual é a palavra {embaralhada} ? ')
+    if tentativa == palavra_sorteada:
+        print(f"Parabéns! Você acertou! A palavra era {palavra_sorteada.upper()}")
+        break  # Encerra o loop
+    elif counter == 3:
+        print(f'Você perdeu, a palavra éra {palavra_sorteada.upper()}')
+        break
+    else:
+        print("Tente novamente. Palavra errada.")
+        counter += 1
