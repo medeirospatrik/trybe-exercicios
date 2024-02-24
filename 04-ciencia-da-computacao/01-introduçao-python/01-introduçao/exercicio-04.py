cafd9676ebef7482cbdef08689ce8eae4469107d
Patrik Medeiros <medeirospatri2@gmail.com>
2024-02-24 04:01:54 -0300

    finalizando

--- 04-ciencia-da-computacao/01-introduçao-python/01-introduçao/exercicio-04.py
+++ 04-ciencia-da-computacao/01-introduçao-python/01-introduçao/exercicio-04.py
@@ -3,6 +3,7 @@
 nome = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]
 
 def maior_nome(names: list):
+    """Recebe uma lista de nomes e retorna o maior nome."""
     big_name = ''
     for name in names:
         if len(name) > len(big_name):
